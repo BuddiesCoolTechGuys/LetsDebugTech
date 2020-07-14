08399a530b6f396bd7536f8b0c6584e2ded68e64
BuddiesCoolTechGuys <67785628+BuddiesCoolTechGuys@users.noreply.github.com>
2020-07-14 22:09:20 +0530

    Restful Tutorial
    This project contains details about Restful API parameters like Path param & Query param.

---
+++ CTG_RestFulParameters/src/main/java/com/ctg/restful/model/StudentInfo.java
@@ -0,0 +1,33 @@
+package com.ctg.restful.model;
+
+public class StudentInfo {
+
+	private String city;
+	private String state;
+	private String country;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
